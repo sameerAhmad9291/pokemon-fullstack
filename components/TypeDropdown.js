7352867c86ae07c7f95cf1b061e088707a96671f
Sameer Ahmad <sameerahmad@Sameers-MacBook-Pro.local>
2024-01-29 17:06:23 +0000

    beautify

--- components/TypeDropdown.js
+++ components/TypeDropdown.js
@@ -24,7 +24,7 @@ export default function TypeDropdown({ onChange }) {
   return (
     <select
       onChange={onChange}
-      className="bg-gray-300 hover:bg-gray-400 px-4 py-2 rounded-md focus:outline-none mx-2"
+      className="bg-gray-300 hover:bg-gray-400 px-4 py-2 rounded-md focus:outline-none mx-2 mt-2"
     >
       <option value={""}> --- Select Type --- </option>
       {pokemonTypes.map(({ id, name }) => {
--- pages/index.js
+++ pages/index.js
@@ -9,24 +9,26 @@ export default function Home() {
   const [type, setType] = useState("");
 
   return (
-    <section className="grid">
-      <div className="grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 mt-10 px-10">
-        {/* Search by name filter */}
-        <input
-          type="text"
-          placeholder="Pokemon Name"
-          value={searchQuery}
-          onChange={(event) => setSearchQuery(event.target.value)}
-          className="px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500 flex-col mx-2"
-        />
+    <main className="mt-1 max-width">
+      <section className="grid">
+        <div className="grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 mt-8 px-10">
+          {/* Search by name filter */}
+          <input
+            type="text"
+            placeholder="Pokemon Name"
+            value={searchQuery}
+            onChange={(event) => setSearchQuery(event.target.value)}
+            className="px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500 flex-col mx-2 mt-2"
+          />
 
-        {/* filter by type */}
-        <TypeDropdown onChange={(event) => setType(event.target.value)} />
+          {/* filter by type */}
+          <TypeDropdown onChange={(event) => setType(event.target.value)} />
 
-        {/* filter by stats */}
-        <StatDropdown onChange={(event) => setStat(event.target.value)} />
-      </div>
-      <PokemonList filters={{ searchQuery, stats: stat, types: type }} />
-    </section>
+          {/* filter by stats */}
+          <StatDropdown onChange={(event) => setStat(event.target.value)} />
+        </div>
+        <PokemonList filters={{ searchQuery, stats: stat, types: type }} />
+      </section>
+    </main>
   );
 }
