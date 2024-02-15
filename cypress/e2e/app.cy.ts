494028091516fb9ead8640ef3db7e25c39e15f05
Sameer Ahmad <sameerahmad@Sameers-MacBook-Pro.local>
2024-02-15 23:46:47 +0000

    testing github action trigger

--- cypress/e2e/app.cy.ts
+++ cypress/e2e/app.cy.ts
@@ -1,4 +1,5 @@
 import { CypressIds } from "../constants";
+const searchQuery = "bl";
 
 describe("Validate Home Page", () => {
   beforeEach(() => {
@@ -9,8 +10,7 @@ describe("Validate Home Page", () => {
     cy.get(CypressIds.pokemonCardId).should("have.length", 151);
   });
 
-  it("should search pokemons by name", () => {
-    const searchQuery = "bl";
+  it(`should search pokemons and list only that startWith ${searchQuery}`, () => {
     cy.get(CypressIds.pokemonSearchInputId).type(searchQuery);
     cy.get(CypressIds.pokemonNameId).should(($el) => {
       const text = $el.text();
